0e63625f462339eddecf42faf93a9c47a428cb68
Arun <arunkushwahaayan@gmail.com>
2024-03-07 18:02:21 +0530

    powerOfNextJs

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -4,13 +4,45 @@ const index = () => {
   return (
     <Fragment>
       <MeetupDetails
-        image="https://nztraveltips.com/wp-content/uploads/2022/01/New-Chums-Beach-900x663.jpg"
+        id="m1"
         title="first meetup"
-        address="kanpur"
-        disription="this is a first meetup"
+        image="https://nztraveltips.com/wp-content/uploads/2022/01/New-Chums-Beach-900x663.jpg"
+        address="Nature"
       />
     </Fragment>
   );
 };
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetups: {
+        id: meetupId,
+        title: "first meetup",
+        image:
+          "https://nztraveltips.com/wp-content/uploads/2022/01/New-Chums-Beach-900x663.jpg",
+        address: "Nature",
+      },
+    },
+  };
+}
 
 export default index;
